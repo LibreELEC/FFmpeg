983a7e16eada19bfeb423ae64e2de7dbadd45497
Matthias Reichl <hias@horus.com>
2026-03-28 12:25:00 +0100

    add sourcepluin-libpostproc from michaelni
    Rebased version of
    https://github.com/michaelni/FFmpeg/tree/sourceplugin-libpostproc-8.0
    
    Signed-off-by: Matthias Reichl <hias@horus.com>

--- fftools/opt_common.c
+++ fftools/opt_common.c
@@ -61,6 +61,8 @@
 #include "libswresample/swresample.h"
 #include "libswresample/version.h"
 
+#include "libpostproc/postprocess.h"
+#include "libpostproc/version.h"
 
 enum show_muxdemuxers {
     SHOW_DEFAULT,
@@ -195,6 +197,7 @@ static void print_all_libs_info(int flags, int level)
     PRINT_LIB_INFO(avfilter,   AVFILTER,   flags, level);
     PRINT_LIB_INFO(swscale,    SWSCALE,    flags, level);
     PRINT_LIB_INFO(swresample, SWRESAMPLE, flags, level);
+    PRINT_LIB_INFO(postproc,   POSTPROC,   flags, level);
 }
 
 static void print_program_info(int flags, int level)
